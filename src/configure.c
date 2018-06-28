346cf412bd1abde00ab47fe17cf5e2865e836647
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-27 22:16:00 -0500

    Clean up and call it a day

--- src/configure.c
+++ src/configure.c
@@ -4,4 +4,3 @@ uint32_t fused_layers;
 uint32_t partitions;
 uint32_t partitions_w;
 uint32_t partitions_h;
-
--- src/top.c
+++ src/top.c
@@ -279,52 +279,54 @@ int main(int argc, char **argv){
    /*test_queue_remove(argc, argv);*/
    /*test_wst(argc, argv);*/
 
-
    this_cli_id = 0;
    total_cli_num = 1;
    init_queues(total_cli_num);
 
    cnn_model* model = load_cnn_model((char*)"models/yolo.cfg", (char*)"models/yolo.weights");
-   model->ftp_para = preform_ftp(2, 2, 4, model->net_para);
+   model->ftp_para = preform_ftp(2, 2, 3, model->net_para);
 
+/*
+   for(int i = 0; i < 2; i++){
+      for(int j = 0; j < 2; j++){
+         printf("------------------(%3d,%3d)----------------\n", i, j);
+         for(int l = 2; l >= 0; l--)
+            print_tile_region(model->ftp_para->output_tiles[model->ftp_para->task_id[i][j]][l]);
+         print_tile_region(model->ftp_para->input_tiles[model->ftp_para->task_id[i][j]][0]);
+      }
+   }
+*/
 
-   int frame_seq = 0;
-   image_holder img = load_image_as_model_input(model, frame_seq);
-   partition_and_enqueue(model, frame_seq);
-
-
-   blob* temp;
-   blob* result;
-   while(1){
-      temp = try_dequeue(task_queue);
-      if(temp == NULL) break;
-      set_model_input(model, (float*)temp->data);
-      forward_partition(model, get_blob_task_id(temp));      
-      result = new_blob_and_copy_data(0, 
+   for(int frame_seq = 0; frame_seq < 4; frame_seq++){
+      image_holder img = load_image_as_model_input(model, frame_seq);
+      partition_and_enqueue(model, frame_seq);
+      blob* temp;
+      blob* result;
+      while(1){
+         temp = try_dequeue(task_queue);
+         if(temp == NULL) break;
+         set_model_input(model, (float*)temp->data);
+         forward_partition(model, get_blob_task_id(temp));      
+         result = new_blob_and_copy_data(0, 
                                       get_model_byte_size(model, model->ftp_para->fused_layers-1), 
                                       (uint8_t*)(get_model_output(model, model->ftp_para->fused_layers-1))
                                      );
-      enqueue(results_pool[this_cli_id], result);
-      copy_blob_meta(result, temp);
-      enqueue(result_queue, result); 
-      free_blob(result);
-      free_blob(temp);
+         copy_blob_meta(result, temp);
+         enqueue(results_pool[this_cli_id], result);
+         /*enqueue(result_queue, result);*/ 
+         free_blob(result);
+         free_blob(temp);
+      }
+
+      enqueue(ready_pool, new_empty_blob(this_cli_id));
+
+      float* fused_output = dequeue_and_merge(model);
+      set_model_input(model, fused_output);
+      forward_all(model, model->ftp_para->fused_layers);   
+      free(fused_output);
+      draw_object_boxes(model, frame_seq);
+      free_image_holder(model, img);
    }
-
-
-   
-   enqueue(ready_pool, new_empty_blob(this_cli_id));
-
-
-
-   float* fused_output = dequeue_and_merge(model);
-   set_model_input(model, fused_output);
-   forward_all(model, model->ftp_para->fused_layers-1);   
-   free(fused_output);
-
-   draw_object_boxes(model, frame_seq);
-   free_image_holder(model, img);
-
 /*
    for(int frame_seq = 0; frame_seq < 4; frame_seq++){
       image_holder img = load_image_as_model_input(model, frame_seq);
@@ -333,19 +335,8 @@ int main(int argc, char **argv){
       free_image_holder(model, img);
    }
 */
-   
 
-/*
 
-   for(int i = 0; i < 2; i++){
-      for(int j = 0; j < 2; j++){
-         printf("------------------(%3d,%3d)----------------\n", i, j);
-         for(int l = 3; l >= 0; l--)
-            print_tile_region(model->ftp_para->output_tiles[model->ftp_para->task_id[i][j]][l]);
-         print_tile_region(model->ftp_para->input_tiles[model->ftp_para->task_id[i][j]][0]);
-      }
-   }
-*/
    return 0;
 }
 
