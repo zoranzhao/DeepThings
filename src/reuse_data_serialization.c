5ea1a3e9495ad6cdacee7a34f34f1f892b020513
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-30 16:45:06 -0500

    Add place_deserialized_data API

--- src/reuse_data_serialization.c
+++ src/reuse_data_serialization.c
@@ -122,7 +122,10 @@ overlapped_tile_data** reuse_data_deserialization(cnn_model* model, uint32_t tas
          overlapped_tile_data* regions_and_data_ptr = regions_and_data_ptr_array[position];
          overlap_index = get_region(regions_and_data_ptr+l, mirror_position);
          if((overlap_index.w>0)&&(overlap_index.h>0)){
-            if(get_size(regions_and_data_ptr+l, mirror_position)>0) free(get_data(regions_and_data_ptr+l, mirror_position));
+            if(get_size(regions_and_data_ptr+l, mirror_position)>0) {
+               free(get_data(regions_and_data_ptr+l, mirror_position));
+               set_size(regions_and_data_ptr+l, mirror_position, 0);
+            }
             uint32_t amount_of_element = overlap_index.w*overlap_index.h*net_para->output_maps[l].c;
             float* data = (float* )malloc(amount_of_element*sizeof(float));
             memcpy(data, serial_data, amount_of_element*sizeof(float)); 
@@ -134,6 +137,48 @@ overlapped_tile_data** reuse_data_deserialization(cnn_model* model, uint32_t tas
    }
 
    return regions_and_data_ptr_array;
+}
+
+void place_deserialized_data(cnn_model* model, uint32_t task_id, overlapped_tile_data** regions_and_data_ptr_array, bool *reuse_data_is_required){
+   ftp_parameters_reuse* ftp_para_reuse = model->ftp_para_reuse;
+   overlapped_tile_data regions_and_data;
+   overlapped_tile_data regions_and_data_to_be_placed;
+   tile_region overlap_index;
+
+   uint32_t i = task_id / (ftp_para_reuse->partitions_w); 
+   uint32_t j = task_id % (ftp_para_reuse->partitions_w);
+   int32_t adjacent_id[4];
+   uint32_t position;
+   uint32_t l;
+   for(position = 0; position < 4; position++) 
+      adjacent_id[position]=-1;
+   /*get the up overlapped data from tile below*/
+   if((i+1)<(ftp_para_reuse->partitions_h)) adjacent_id[0] = ftp_para_reuse->task_id[i+1][j];
+   /*get the left overlapped data from tile on the right*/
+   if((j+1)<(ftp_para_reuse->partitions_w)) adjacent_id[1] = ftp_para_reuse->task_id[i][j+1];
+   /*get the bottom overlapped data from tile above*/
+   if(i>0) adjacent_id[2] = ftp_para_reuse->task_id[i-1][j];
+   /*get the right overlapped data from tile on the left*/
+   if(j>0) adjacent_id[3] = ftp_para_reuse->task_id[i][j-1];
+
+   for(l = 0; l < ftp_para_reuse->fused_layers-1; l ++){
+      for(position = 0; position < 4; position++){
+         if(adjacent_id[position]==-1) continue;
+         if(reuse_data_is_required[position]==false) continue;
+         uint32_t mirror_position = (position + 2)%4;
+         regions_and_data = ftp_para_reuse->output_reuse_regions[adjacent_id[position]][l];
+         overlap_index = get_region(&regions_and_data, mirror_position);
+         if((overlap_index.w>0)&&(overlap_index.h>0)){
+            regions_and_data_to_be_placed = regions_and_data_ptr_array[position][l];
+            uint32_t size = get_size(&regions_and_data_to_be_placed, mirror_position);
+            float* data = get_data(&regions_and_data_to_be_placed, mirror_position);
+            set_data(&regions_and_data, mirror_position, data);
+            set_size(&regions_and_data, mirror_position, size);
+         }
+         ftp_para_reuse->output_reuse_regions[adjacent_id[position]][l] = regions_and_data;
+      }
+   }
+
 }
 
 void free_overlapped_tile_data_ptr_array(overlapped_tile_data** regions_and_data_ptr_array){
