0dab028aecdcb023daf8d4c678c928d4067a6cf6
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-30 21:10:36 -0500

    Add partition dimensional parameters

--- include/configure.h
+++ include/configure.h
@@ -7,11 +7,17 @@
 #define PARTITIONS_H_MAX 6
 #define PARTITIONS_MAX 36
 #define THREAD_NUM 1
-#define DATA_REUSE 1
+#define DATA_REUSE 0
 
 #define DEBUG_INFERENCE 0
 #define DEBUG_FTP 0
 #define DEBUG_SERIALIZATION 0
+
+#define FUSED_LAYERS 4
+#define PARTITIONS_W 3
+#define PARTITIONS_H 3
+
+
 
 #include <stdint.h>
 
