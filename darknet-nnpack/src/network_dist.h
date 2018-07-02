3b3d2537421ae3e3eece07e615c7949fc7d7df3c
Zhuoran Zhao <zoranzhao@gmail.com>
2018-07-01 22:35:58 -0500

    Add darknet

---
+++ darknet-nnpack/src/network_dist.h
@@ -0,0 +1,14 @@
+//Adding a distributed prediction network
+#ifndef NETWORK_DIST_H
+#define NETWORK_DIST_H
+#include "darknet.h"
+
+#include "image.h"
+#include "layer.h"
+#include "data.h"
+#include "tree.h"
+#include "network.h"
+
+
+#endif
+
