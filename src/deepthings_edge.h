9b8230c0ebd2ba435610453502ed6d469ed6caaa
Zhuoran Zhao <zoranzhao@gmail.com>
2019-01-18 18:58:26 -0600

    Export init function

--- src/deepthings_edge.h
+++ src/deepthings_edge.h
@@ -3,6 +3,7 @@
 #include "darkiot.h"
 #include "configure.h"
 
+device_ctxt* deepthings_edge_init(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
 void deepthings_stealer_edge(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
 void deepthings_victim_edge(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
 
