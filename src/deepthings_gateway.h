75008239e16647163012fff333c46fcbaa4fa895
Zhuoran Zhao <zoranzhao@gmail.com>
2019-01-18 19:00:28 -0600

    Export initialization functions

--- src/deepthings_gateway.h
+++ src/deepthings_gateway.h
@@ -2,5 +2,6 @@
 #define DEEPTHINGS_GATEWAY_H
 #include "darkiot.h"
 #include "configure.h"
+device_ctxt* deepthings_gateway_init(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t total_edge_number, const char** addr_list);
 void deepthings_gateway(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t total_edge_number, const char** addr_list);
 #endif
