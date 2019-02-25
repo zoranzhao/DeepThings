92625642628076d8f5a4fa8fb2081c463f3cd5fb
Zhuoran Zhao <zoranzhao@gmail.com>
2019-02-24 18:28:00 -0600

    Export more useful functions for edge/gateway devices in header file

--- src/deepthings_edge.h
+++ src/deepthings_edge.h
@@ -2,9 +2,10 @@
 #define DEEPTHINGS_EDGE_H
 #include "darkiot.h"
 #include "configure.h"
-
 device_ctxt* deepthings_edge_init(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
 void deepthings_stealer_edge(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
 void deepthings_victim_edge(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
-
+void partition_frame_and_perform_inference_thread(void *arg);
+void steal_partition_and_perform_inference_thread(void *arg);
+void deepthings_serve_stealing_thread(void *arg);
 #endif
